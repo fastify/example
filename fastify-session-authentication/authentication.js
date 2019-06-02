cd7e9a8afa1eaf59074ecef9e7611499b3009260
Denis Fäcke <serayaeryn@gmail.com>
2019-06-02 15:50:35 +0200

    expose plugin instead of app

--- fastify-session-authentication/authentication.js
+++ fastify-session-authentication/authentication.js
@@ -7,61 +7,57 @@ const fastifyFormbody = require('fastify-formbody')
 
 const { loginPage, defaultPage } = require('./html')
 
-// create a fastify instance
-const app = fastify()
-
-// register the required plugins
-app.register(fastifyFormbody)
-app.register(fastifyCookie)
-app.register(fastifySession, {
-  cookieName: 'sessionId',
-  secret: 'a secret with minimum length of 32 characters',
-  cookie: { secure: false },
-  expires: 1800000
-})
-
-// add a login route that returns a login page
-app.get('/login', (request, reply) => {
-  reply.type('text/html')
-  reply.send(loginPage())
-})
-
-// add a login route that handles the actual login
-app.post('/login', (request, reply) => {
-  const { email, password } = request.body
-
-  if (password === 'abcdef') {
-    request.session.authenticated = true
-    reply.redirect('/')
-  } else {
-    reply.redirect(401, '/login')
-  }
-});
-
-app.get('/', (request, reply) => {
-  reply.type('text/html')
-  reply.send(defaultPage(request.session.authenticated))
-});
-
-// add a logout route
-app.get('/logout', (request, reply) => {
-  if (request.session.authenticated) {
-    request.destroySession((err) => {
-      if (err) {
-        reply.status(500)
-        reply.send('Internal Server Error')
-      } else {
-        reply.redirect('/')
-      }
-    })
-  } else {
-    reply.redirect('/')
-  }
-});
-
-// start the server
-module.exports = app.listen(3000)
-  .then(() => {
-    console.log(`server started on port 3000`)
-    return app
+function plugin (instance, options, next) {
+  // register the required plugins
+  instance.register(fastifyFormbody)
+  instance.register(fastifyCookie)
+  instance.register(fastifySession, {
+    cookieName: 'sessionId',
+    secret: 'a secret with minimum length of 32 characters',
+    cookie: { secure: false },
+    expires: 1800000
   })
+
+  // add a login route that returns a login page
+  instance.get('/login', (request, reply) => {
+    reply.type('text/html')
+    reply.send(loginPage())
+  })
+  
+  // add a login route that handles the actual login
+  instance.post('/login', (request, reply) => {
+    const { email, password } = request.body
+  
+    if (password === 'abcdef') {
+      request.session.authenticated = true
+      reply.redirect('/')
+    } else {
+      reply.redirect(401, '/login')
+    }
+  });
+
+  instance.get('/', (request, reply) => {
+    reply.type('text/html')
+    reply.send(defaultPage(request.session.authenticated))
+  });
+  
+  // add a logout route
+  instance.get('/logout', (request, reply) => {
+    if (request.session.authenticated) {
+      request.destroySession((err) => {
+        if (err) {
+          reply.status(500)
+          reply.send('Internal Server Error')
+        } else {
+          reply.redirect('/')
+        }
+      })
+    } else {
+      reply.redirect('/')
+    }
+  });
+
+  next()
+}
+
+module.exports = plugin
--- fastify-session-authentication/authentication.test.js
+++ fastify-session-authentication/authentication.test.js
@@ -1,101 +1,119 @@
 'use strict'
 
 const { test } = require('tap')
-const server = require('./authentication')
+const plugin = require('./authentication')
 const requestCallback = require('request')
+const fastify = require('fastify')
 
-server.then((app) => {
-  app.server.unref()
+test('should be able to login', async (t) => {
+  t.plan(3)
   
-  test('should be able to login', async (t) => {
-    t.plan(3)
-    
-    const result0 = await performLogin('abcdef')
-    const result1 = await toHomePage(result0.sessionCookie)
-    
-    t.equals(result0.location, '/')
-    t.ok(result1.body.includes('logged in'))
-    t.equals(result1.sessionCookie, result0.sessionCookie)
-  })
-  
-  test('should not be able to login with wrong password', async (t) => {
-    t.plan(2)
-    
-    const { location, statusCode } = await performLogin('123456')
-  
-    t.equals(location, '/login')
-    t.equals(statusCode, 401)
-  })
+  const { port } = await startServer()
 
-  test('should be not logged in', async (t) => {
-    t.plan(2)
-    
-    const { sessionCookie, body } = await toHomePage()
+  const result0 = await performLogin(port, 'abcdef')
+  const result1 = await toHomePage(port, result0.sessionCookie)
   
-    t.ok(body.includes('please login'))
-    t.ok(sessionCookie.includes('sessionId'))
-  })
+  t.equals(result0.location, '/')
+  t.ok(result1.body.includes('logged in'))
+  t.equals(result1.sessionCookie, result0.sessionCookie)
+})
   
-  test('should be able to logout', async (t) => {
-    t.plan(5)
-    
-    const result0 = await performLogin('abcdef')
-    const result1 = await toHomePage(result0.sessionCookie)
-    const result2 = await logout(result0.sessionCookie)
-    const result3 = await toHomePage(result2.sessionCookie)
-    
-    t.equals(result0.location, '/')
-    t.ok(result1.body.includes('logged in'))
-    t.ok(result3.body.includes('please login'))
-    t.ok(result3.sessionCookie === result2.sessionCookie)
-    t.ok(result0.sessionCookie !== result2.sessionCookie)
-  })
+test('should not be able to login with wrong password', async (t) => {
+  t.plan(2)
+
+  const { port } = await startServer()
   
-  test('should be able to call logout if not logged in', async (t) => {
-    t.plan(2)
-    
-    const result0 = await logout()
-    const result1 = await toHomePage(result0.sessionCookie)
-    
-    t.ok(result1.body.includes('please login'))
-    t.ok(result1.sessionCookie === result0.sessionCookie)
-  })
+  const { location, statusCode } = await performLogin(port, '123456')
+
+  t.equals(location, '/login')
+  t.equals(statusCode, 401)
+})
+
+test('should be not logged in', async (t) => {
+  t.plan(2)
+
+  const { port } = await startServer()
+  
+  const { sessionCookie, body } = await toHomePage(port)
+
+  t.ok(body.includes('please login'))
+  t.ok(sessionCookie.includes('sessionId'))
+})
+
+test('should be able to logout', async (t) => {
+  t.plan(5)
+
+  const { port } = await startServer()
+  
+  const result0 = await performLogin(port, 'abcdef')
+  const result1 = await toHomePage(port, result0.sessionCookie)
+  const result2 = await logout(port, result0.sessionCookie)
+  const result3 = await toHomePage(port, result2.sessionCookie)
+  
+  t.equals(result0.location, '/')
+  t.ok(result1.body.includes('logged in'))
+  t.ok(result3.body.includes('please login'))
+  t.ok(result3.sessionCookie === result2.sessionCookie)
+  t.ok(result0.sessionCookie !== result2.sessionCookie)
+})
+
+test('should be able to call logout if not logged in', async (t) => {
+  t.plan(2)
+
+  const { port } = await startServer()
+  
+  const result0 = await logout(port)
+  const result1 = await toHomePage(port, result0.sessionCookie)
+  
+  t.ok(result1.body.includes('please login'))
+  t.ok(result1.sessionCookie === result0.sessionCookie)
 })
 
 test('should be able to request login page', async (t) => {
   t.plan(1)
-  const { statusCode } = await toLoginPage()
+  
+  const { port } = await startServer()
+
+  const { statusCode } = await toLoginPage(port)
 
   t.equals(statusCode, 200)
 })
 
-async function toHomePage(cookie) {
+async function startServer () {
+  const app = fastify()
+  app.register(plugin)
+  app.server.unref()
+  await app.listen(0)
+  return app.server.address()
+}
+
+async function toHomePage(port, cookie) {
   return request({
-    url: 'http://localhost:3000/',
+    url: `http://localhost:${port}/`,
     method: 'GET',
     headers: { cookie }
   });
 }
 
-async function performLogin(password) {
+async function performLogin(port, password) {
   return request({
-    url: 'http://localhost:3000/login',
+    url: `http://localhost:${port}/login`,
     method: 'POST',
     form: { email: 'test@test.de', password },
     followRedirects: false
   });
 }
 
-async function toLoginPage() {
+async function toLoginPage(port) {
   return request({
-    url: 'http://localhost:3000/login',
+    url: `http://localhost:${port}/login`,
     method: 'GET'
   });
 }
 
-async function logout(cookie) {
+async function logout(port, cookie) {
   return request({
-    url: 'http://localhost:3000/logout',
+    url: `http://localhost:${port}/logout`,
     method: 'GET',
     followRedirects: false,
     headers: { cookie }
