8b2f56e8873a93972aa9ac1ad2aa932458358f61
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-03-07 09:53:45 +0200

    setup tap

--- writting-tests/with-tap/app.js
+++ writting-tests/with-tap/app.js
@@ -1,12 +1,15 @@
-const fastify = require("fastify")
+const fastify = require("fastify");
 
-function build(opts={}) {
-    const app = fastify(opts)
-    app.get('/',async function (request,reply) {
-        return {Test:'Tests'}
-    })
-    return app
+function build(opts = {}) {
+  const app = fastify(opts);
+  app.get("/", async function (request, reply) {
+    return { Test: "Tests" };
+  });
+
+  app.post("/:id", async function (request, reply) {
+    return { id: id };
+  });
+  return app;
 }
 
-
-module.exports = build
+module.exports = build;
---
+++ writting-tests/with-tap/tests/app.test.js
@@ -0,0 +1,25 @@
+const fastify = require("fastify");
+const { test } = require("tap");
+const build = require("../app");
+
+// Test the default route
+test('requests the "/" route', async (t) => {
+  const app = build();
+
+  const response = await app.inject({
+    method: "GET",
+    url: "/",
+  });
+
+  t.strictEqual(response.statusCode, 200, "returns a status code of 200");
+});
+
+// How test get request with query parameters
+test("should get params from url", async (t) => {
+  try {
+    const res = await fastify.inject({ method: "POST", url: "/1232" });
+    console.log(res.statusCode);
+  } catch (error) {
+    console.error({ error });
+  }
+});
