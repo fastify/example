dbc46f5287e3747ce63ed974436c920f935092e3
Makuza Mugabo Verite <mugaboverite@gmail.com>
2021-03-08 19:06:08 +0200

    added use strict

--- writting-tests/with-tap/server.js
+++ writting-tests/with-tap/server.js
@@ -1,14 +1,15 @@
-const server = require('./app')({
-    logger:{
-        level:'info',
-        prettyPrint:true
-    }
-})
+"use strict";
 
+const server = require("./app")({
+  logger: {
+    level: "info",
+    prettyPrint: true,
+  },
+});
 
-server.listen(3000,(err,address)=>{
-    if(err){
-        console.log(err)
-        process.exit(1)
-    }
-})
+server.listen(3000, (err, address) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+});
