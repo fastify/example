ed5881c9a8f5fef869e26d38617f4c67f60c4b35
RafaelGSS <rafael.nunu@hotmail.com>
2021-09-23 20:06:24 -0300

    feat: add tests example

---
+++ tests/app.js
@@ -0,0 +1,42 @@
+const Fastify = require('fastify')
+const fp = require('fastify-plugin')
+const fastifyMongodb = require('fastify-mongodb')
+const mongodb = require('mongodb')
+
+function app(fastify, config, done) {
+  fastify.register(fastifyMongodb, { client: config.mongodb.client })
+  fastify.get('/', async function (_req, reply) {
+    const db = this.mongo.client.db()
+    const results = await db.collection('examples').find()
+    return reply.send(results)
+  })
+
+  fastify.addHook('onClose', async () => {
+    await config.mongodb.client.close()
+  })
+  done()
+}
+
+async function server() {
+  const fastify = Fastify({ logger: true })
+  const database = process.env.MONGO_URI || 'mongodb://localhost/prod'
+  const client = await mongodb.MongoClient.connect(database)
+  fastify.register(
+    fp(app),
+    {
+      mongodb: {
+        client,
+        database,
+      }
+    }
+  )
+
+  fastify.listen(3000)
+}
+
+if (require.main === module) {
+  server()
+} else {
+  module.exports = app
+}
+
---
+++ tests/database-connection/mongodb-setup.js
@@ -0,0 +1,63 @@
+'use strict'
+
+const Fastify = require('fastify')
+const fp = require('fastify-plugin')
+
+const clean = require('mongo-clean')
+const { MongoClient } = require('mongodb')
+const { beforeEach, teardown } = require('tap')
+
+const App = require('../app')
+
+const url = 'mongodb://localhost/prod'
+const database = 'tests'
+
+let client
+
+beforeEach(async function () {
+  if (!client) {
+    client = await MongoClient.connect(url, {
+      w: 1,
+      useNewUrlParser: true
+    })
+  }
+  await clean(client.db(database))
+})
+
+teardown(async function () {
+  if (client) {
+    await client.close()
+    client = null
+  }
+})
+
+// Fill in this config with all the configurations
+// needed for testing the application
+function config () {
+  return {
+    mongodb: {
+      client,
+      database
+    }
+  }
+}
+
+// automatically build and tear down our instance
+function build (t) {
+  const app = Fastify()
+
+  // fastify-plugin ensures that all decorators
+  // are exposed for testing purposes, this is
+  // different from the production setup
+  app.register(fp(App), config())
+
+  // tear down our app after we are done
+  t.teardown(app.close.bind(app))
+
+  return app
+}
+
+module.exports = {
+  config,
+  build
+}
